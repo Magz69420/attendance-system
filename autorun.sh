e2f55d536ed9a1eedafe04402b779d7df35153aa
Magz69420 <manasvi4220@gmail.com>
2024-05-16 19:52:34 +0530

    update

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /
 cd home/Denji/attendnace-system
-sudo get_faces_from_camera_tkinter.py &
-sudo features_extraction_to_csv.py &
-attendance_taker.py
+sudo python get_faces_from_camera_tkinter.py &
+sudo python features_extraction_to_csv.py &
+sudo python attendance_taker.py &
 
 exit 0
 cd /
