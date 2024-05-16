974c82ca0549927ddacdae8dab72572fea9f1cdf
Magz69420 <manasvi4220@gmail.com>
2024-05-16 20:02:38 +0530

    attendance.db

--- autorun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /
-cd home/Denji/attendnace-system
-sudo python get_faces_from_camera_tkinter.py &
-sudo python features_extraction_to_csv.py &
-sudo python attendance_taker.py &
-
-exit 0
-cd /
