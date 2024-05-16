8ebfea2c9ef790f47189787bce9f00c537650132
Magz69420 <manasvi4220@gmail.com>
2024-05-16 21:27:17 +0530

    update

--- autopush.sh
+++ autopush.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-cd $(dirname ${BASH_SOURCE[0]})
+# Set the remote URL to use SSH authentication
+git remote set-url origin git@github.com:Magz69420/attendance-system.git
 
+# Navigate to the directory containing the script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Check if there are any changes in the repository
 if [[ -n $(git status -s) ]]; then
     echo "Changes found. Pushing changes..."
     git add -A && git commit -m 'update' && git push
